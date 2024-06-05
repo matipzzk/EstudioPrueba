f3314063f019d85055cf40480cb8a7353fe0bc00
matipzzk <ma.imil@duocuc.cl>
2024-06-05 17:16:56 -0400

    V2Final

--- Estudio_Prueba/matrices.py
+++ Estudio_Prueba/matrices.py
@@ -3,3 +3,13 @@ import os
 matriz = [[0]*4 for i in range(3)]
 
 
+# Ingresa los valors de la matriz según usuario 
+for i in range(3):
+    for j in range(4):
+        matriz[i][j] = int(input(f"Ingrese el número [{i}][{j}]: "))
+
+# imprime la matriz 
+for i in range (3):
+    for j in range(4):
+        print(matriz[i][j], end = " ")
+    print()
